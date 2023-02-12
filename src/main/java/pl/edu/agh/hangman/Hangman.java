2866b2755399c7959a0cfe257361d7903a1701b1
student4 <student4@pcoip.ki.agh.edu.pl>
2023-02-12 14:15:35 +0100

    first commit

--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -1,6 +1,42 @@
 package pl.edu.agh.hangman;
 
+import java.util.Scanner;
+
 public class Hangman {
+
+    //lista slow
+    private static String[] slowa = {"komputer", "pies", "kot", "samochod"};
+    // losowanie slowa
+    private static String losoweSlowo = slowa[(int) (Math.random() * slowa.length)];
+    //ukrycie losowego slowa
+    private static String ukryteSlowo = new String(new char[losoweSlowo.length()]).replace("\0", "_");
+
+    private static int count = 0;
+
+    //metoda uzupelniajaca slowo
+    public static void sprawdzSlowo(String slowo) {
+        String slowoPoSprawdzeniu = "";
+        for (int i = 0; i < losoweSlowo.length(); i++) {
+            if (losoweSlowo.charAt(i) == slowo.charAt(0)) {
+                slowoPoSprawdzeniu += slowo.charAt(0);
+            } else if (ukryteSlowo.charAt(i) != '_') {
+                slowoPoSprawdzeniu += losoweSlowo.charAt(i);
+            } else {
+                slowoPoSprawdzeniu += "_";
+            }
+        }
+
+        if (ukryteSlowo.equals(slowoPoSprawdzeniu)) {
+            count++;
+            System.out.println(HANGMANPICS[count]);
+        } else {
+            ukryteSlowo = slowoPoSprawdzeniu;
+        }
+
+        if (ukryteSlowo.equals(losoweSlowo)) {
+            System.out.println("Wygrales");
+        }
+    }
 
     public static final String[] HANGMANPICS = new String[]{
             "  +---+\n" +
@@ -55,5 +91,28 @@ public class Hangman {
     };
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        while (count < HANGMANPICS.length && ukryteSlowo.contains("_")) {
+        System.out.println("Podaj litere");
+        System.out.println(ukryteSlowo);
+        String slowo = scanner.next();
+        sprawdzSlowo(slowo);
+        }
+        scanner.close();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
